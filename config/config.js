55290ddd5fd21101e363bc6f3d824d905f07940d
Truong Quoc Kiet <tqk03gl@gmail.com>
2025-03-12 12:02:49 +0700

    api key

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    API_KEY: "AIzaSyAJLU_ABt6K5Vc_Mkta8SlTqZHvFChkIDQ", 
+    API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+};
+
+// Xuất cấu hình để file khác sử dụng
+export default CONFIG;
